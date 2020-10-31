79ea388f071eb95a1b626e5c2e7fb96179d86a6b
NinoM4ster <ninom4ster@gmail.com>
2020-10-30 21:34:19 -0300

    list all platforms codes for reference

---
+++ src/main.go
@@ -0,0 +1,59 @@
+package main
+
+/*
+
+aixppc64 (aix/ppc64)
+
+and386 (android/386)
+and64 (android/amd64)
+andarm (android/arm)
+andarm64 (android/arm64)
+
+dar64 (darwin/amd64)
+dararm64 (darwin/arm64)
+
+dra64 (dragonfly/amd64)
+
+fre386 (freebsd/386)
+fre64 (freebsd/amd64)
+frearm (freebsd/arm)
+frearm64 (freebsd/arm64)
+
+ill64 (illumos/amd64)
+
+jswasm (js/wasm)
+
+lin386 (linux/386)
+lin64 (linux/amd64)
+linarm (linux/arm)
+linarm64 (linux/arm64)
+linmips (linux/mips)
+linmips64 (linux/mips64)
+linmips64le (linux/mips64le)
+linmipsle (linux/mipsle)
+linppc64 (linux/ppc64)
+linppc64le (linux/ppc64le)
+linriscv64 (linux/riscv64)
+lins390x (linux/s390x)
+
+net386 (netbsd/386)
+net64 (netbsd/amd64)
+netarm (netbsd/arm)
+netarm64 (netbsd/arm64)
+
+ope386 (openbsd/386)
+ope64 (openbsd/amd64)
+opearm (openbsd/arm)
+opearm64 (openbsd/arm64)
+
+pla386 (plan9/386)
+pla64 (plan9/amd64)
+plaarm (plan9/arm)
+
+sol64 (solaris/amd64)
+
+win386 (windows/386)
+win64 (windows/amd64)
+winarm (windows/arm)
+
+*/
