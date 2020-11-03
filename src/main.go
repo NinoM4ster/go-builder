3d57826362e0874cb76d79a31597bb9f6c16041b
NinoM4ster <ninom4ster@gmail.com>
2020-11-03 02:33:53 -0300

    bump version & cleanup

--- src/main.go
+++ src/main.go
@@ -9,64 +9,6 @@ import (
 	"strings"
 )
 
-/*
-
-aixppc64 (aix/ppc64)
-
-and386 (android/386)
-and64 (android/amd64)
-andarm (android/arm)
-andarm64 (android/arm64)
-
-dar64 (darwin/amd64)
-dararm64 (darwin/arm64)
-
-dra64 (dragonfly/amd64)
-
-fre386 (freebsd/386)
-fre64 (freebsd/amd64)
-frearm (freebsd/arm)
-frearm64 (freebsd/arm64)
-
-ill64 (illumos/amd64)
-
-jswasm (js/wasm)
-
-lin386 (linux/386)
-lin64 (linux/amd64)
-linarm (linux/arm)
-linarm64 (linux/arm64)
-linmips (linux/mips)
-linmips64 (linux/mips64)
-linmips64le (linux/mips64le)
-linmipsle (linux/mipsle)
-linppc64 (linux/ppc64)
-linppc64le (linux/ppc64le)
-linriscv64 (linux/riscv64)
-lins390x (linux/s390x)
-
-net386 (netbsd/386)
-net64 (netbsd/amd64)
-netarm (netbsd/arm)
-netarm64 (netbsd/arm64)
-
-ope386 (openbsd/386)
-ope64 (openbsd/amd64)
-opearm (openbsd/arm)
-opearm64 (openbsd/arm64)
-
-pla386 (plan9/386)
-pla64 (plan9/amd64)
-plaarm (plan9/arm)
-
-sol64 (solaris/amd64)
-
-win386 (windows/386)
-win64 (windows/amd64)
-winarm (windows/arm)
-
-*/
-
 var (
 	targetsFlag    string
 	targets        []string
@@ -74,7 +16,7 @@ var (
 )
 
 func main() {
-	fmt.Println("go-builder v1.1 by github.com/NinoM4ster" + "\n")
+	fmt.Println("go-builder v1.2 by github.com/NinoM4ster" + "\n")
 	flag.StringVar(&targetsFlag, "t", "", "Target platforms: <win64[,lin386[,linarm64]]>")
 	flag.StringVar(&source, "s", "", "Source: <main.go>")
 	flag.StringVar(&output, "o", "", "Output: <bin/MyApp_%>")
